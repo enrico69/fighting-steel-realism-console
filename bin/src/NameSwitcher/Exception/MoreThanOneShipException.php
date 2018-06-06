4c04273aa1726c644b70a1e45170d0e0fcf3164e
Eric COURTIAL <e.courtial30@gmail.com>
2018-06-05 21:54:24 -0400

    Improved menu, added version and added specific exceptions

---
+++ bin/src/NameSwitcher/Exception/MoreThanOneShipException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by Eric COURTIAL.
+ * @author <e.courtial30@gmail.com>
+ * Date: 18-06-05
+ */
+namespace App\NameSwitcher\Exception;
+
+/**
+ * Class MoreThanOneShipException
+ * @package App\NameSwitcher\Exception
+ */
+class MoreThanOneShipException extends \Exception
+{
+
+}
---
+++ bin/src/NameSwitcher/Exception/NoShipException.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by Eric COURTIAL.
+ * @author <e.courtial30@gmail.com>
+ * Date: 18-06-05
+ */
+namespace App\NameSwitcher\Exception;
+
+/**
+ * Class NoShip
+ *
+ * @package App\NameSwitcher\Exception
+ */
+class NoShipException extends \Exception
+{
+
+}
